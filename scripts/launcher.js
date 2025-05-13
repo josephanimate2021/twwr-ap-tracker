3a36f3ef95064bda191d05c26ca3a4e0e1a76456
Joseph <100003036+josephanimate2021@users.noreply.github.com>
2025-05-12 22:10:34 -0400

    Update launcher.js

--- scripts/launcher.js
+++ scripts/launcher.js
@@ -149,7 +149,7 @@ function getFlagString() {
  * @returns {string}
  */
 function trackerLink() {
-  return `./tracker.html?f=${getFlagString()}&g=${$("#apConfig").data("startingGear") || 0}&p=0&v=master&c=1&${$("#apConfig").serialize()}`
+  return `${location.origin}/tracker.html?f=${getFlagString()}&g=${$("#apConfig").data("startingGear") || 0}&p=0&v=master&c=1&${$("#apConfig").serialize()}`
 }
 
 /**
