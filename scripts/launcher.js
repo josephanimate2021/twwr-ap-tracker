0fc9f331f440251e27ea71f4e81ab1c4a808d364
Joseph <100003036+josephanimate2021@users.noreply.github.com>
2025-05-12 21:40:42 -0400

    Update launcher.js

--- scripts/launcher.js
+++ scripts/launcher.js
@@ -71,7 +71,7 @@ function applyAPSettings(form) {
               break;
             } case "Connected": {
               success = true;
-              displayMessage(`Successfuly modified settings from AP!`);
+              displayMessage(`Successfuly connected to AP and modified the settings from there. You are safe to either Launch the Tracker for copy the Tracker Link which can be used for a variety of things.`);
               submutBtn.text(origText);
               submutBtn.removeAttr("disabled");
               for (const btn of document.getElementById('launcherButtons').children) {
