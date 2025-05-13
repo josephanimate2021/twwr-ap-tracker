de17ef9c52e7ad3f9e8b3784d4c8861e808d9a6c
Joseph Crosman <josephcrosmanplays@hotmail.com>
2025-05-12 20:40:23 -0400

    Launcher is getting pretty close to AP Material

--- scripts/launcher.js
+++ scripts/launcher.js
@@ -1,62 +1,16 @@
-const currentVersion = '1.8.0';
-var startingGear = 0;
+// Script for the twwr ap tracker launcher.
 
-function parseFlags(bits, ids) {
-  for (var i = 0; i < ids.length; i++) {
-    var element = document.getElementById(ids[i]);
-    if (element) {
-      element.checked = bits[0];
-    }
-    bits.shift();
-  }
+/**
+ * Takes a word and capitalizes the beginning of it's letter.
+ * @param {string} g 
+ * @returns {string}
+ */
+function capWord(g) {
+  const rest = g.substring(1);
+  const beg = g.slice(0, -rest.length);
+  return beg.toUpperCase() + rest;
 }
 
-function parseComboBox(bits, id) {
-  var byteValue = getValueOfBits(bits, 8);
-  var element = document.getElementById(id);
-  element.selectedIndex = byteValue;
-}
-
-function parseSpinBox(bits, id, minVal, maxVal) {
-  var numBits = (maxVal - minVal).toString(2).length;
-  var byteValue = getValueOfBits(bits, numBits);
-  var element = document.getElementById(id);
-  if (element) {
-    element.value = byteValue;
-  }
-}
-
-function setStartingGear(bits) {
-  var numBits = regularItems.length + progressiveItems.length * 2;
-  startingGear = getValueOfBits(bits, numBits);
-}
-
-function getValueOfBits(bits, count) {
-  var value = 0;
-  for (var i = 0; i < count; i++) {
-    if (bits[0]) {
-      value += Math.pow(2, i);
-    }
-    bits.shift();
-  }
-  return value;
-}
-
-function getBitString(bits) {
-  var bitString = [];
-  for (var i = 0; i < bits.length; i++) {
-    var curByte = bits.charCodeAt(i);
-    for (var j = 0; j < 8; j++) {
-      if (curByte % 2 == 1) {
-        bitString.push(true);
-      } else {
-        bitString.push(false);
-      }
-      curByte = Math.floor(curByte / 2);
-    }
-  }
-  return bitString;
-}
 /**
  * Checks for an existant AP server connection before redirecting the user to the actual tracker.
  * @param {HTMLButtonElement} submutBtn 
@@ -107,73 +61,6 @@ function checkConnection(submutBtn) {
   } else handleError('Please type in a valid AP Server Address');
 }
 
-function applyflags(element) {
-  try {
-    var decodedData = atob(document.getElementById('flags').value);
-    var version = '';
-    var bits = '';
-
-    while (decodedData.charCodeAt(0) != 0 && decodedData.length > 0) {
-      version += decodedData[0];
-      decodedData = decodedData.substring(1);
-    }
-
-    if (/^[0-9a-f._]+$/.test(version) && !version.includes(currentVersion)) {
-      showBrokenPermalink(element, true);
-      return;
-    }
-
-    if (decodedData.length > 0) {
-      decodedData = decodedData.substring(1); // space between version and seed
-    }
-
-    while (decodedData.charCodeAt(0) != 0 && decodedData.length > 0) {
-      decodedData = decodedData.substring(1); // seed
-    }
-
-    if (decodedData.length > 0) {
-      decodedData = decodedData.substring(1); // space between seed and flags
-      bits = getBitString(decodedData);
-
-      parseFlags(bits, ['dungeons', 'great_fairies', 'puzzle_secret_caves', 'combat_secret_caves', 'short_sidequests', 'long_sidequests', 'spoils_trading', 'minigames']);
-      parseFlags(bits, ['free_gifts', 'mail', 'platforms_rafts', 'submarines', 'eye_reef_chests', 'big_octos_gunboats', 'triforce_charts', 'treasure_charts']);
-      parseFlags(bits, ['expensive_purchases', 'misc', 'tingle_chests', 'battlesquid', 'savage_labyrinth', 'island_puzzles', 'key_lunacy']);
-      parseComboBox(bits, 'randomize_entrances');
-      parseFlags(bits, ['randomize_charts', '' /* randomize_starting_island */, '' /* swift_sail */, '' /* instant_text_boxes */, '' /* reveal_full_sea_chart */]);
-      parseComboBox(bits, 'num_starting_triforce_shards');
-      parseFlags(bits, ['' /* add_shortcut_warps_between_dungeons */, '' /* generate_spoiler_log */]);
-      parseComboBox(bits, 'sword_mode');
-      parseFlags(bits, ['skip_rematch_bosses', 'race_mode']);
-      parseFlags(bits, ['' /* randomize_bgm */, '' /* disable_tingle_chests_with_tingle_bombs */, '' /* randomize_enemy_palettes */, '' /* remove_title_and_ending_videos */]);
-      setStartingGear(bits);
-      parseSpinBox(bits, '' /* starting_pohs */, 0, 44);
-      parseSpinBox(bits, '' /* starting_hcs */, 0, 6);
-
-      $(element).notify('Settings applied from the Permalink.', {
-        autoHideDelay: 5000,
-        className: 'success',
-        position: 'top center'
-      });
-    } else {
-      showBrokenPermalink(element, false);
-    }
-  }
-  catch (err) {
-    showBrokenPermalink(element, false);
-  }
-}
-
-function showBrokenPermalink(element, wrongVersion) {
-  if (wrongVersion) {
-    var notificationMessage = 'Settings could not be applied. Permalink is not compatible with Wind Waker Randomizer ' + currentVersion + '.';
-  } else {
-    var notificationMessage = 'Settings could not be applied from the Permalink.';
-  }
-  displayMessage(notificationMessage, element, {
-    className: 'error',
-  });
-}
-
 /**
  * Displays a message using the jquery notify option based off of user input
  * @param {string} msg 
@@ -189,6 +76,10 @@ function displayMessage(msg, element, options = {}) {
   }));
 }
 
+/**
+ * Gets a flag string out of all buttons
+ * @returns {string}
+ */
 function getFlagString() {
   var flagNames = ['D', 'GF', 'PSC', 'CSC', 'SSQ', 'LSQ', 'ST', 'MG', 'FG', 'MAI', 'PR', 'SUB', 'ERC', 'BOG', 'TRI', 'TRE', 'EP', 'MIS', 'TIN', 'KL', 'REN', 'RCH', 'SWO', 'SRB', 'STS', 'RM', 'SAV', 'BSM', 'IP'];
   var buttonNames = ['dungeons', 'great_fairies', 'puzzle_secret_caves', 'combat_secret_caves', 'short_sidequests', 'long_sidequests', 'spoils_trading', 'minigames',
@@ -211,37 +102,31 @@ function getFlagString() {
   return result;
 }
 
-function openTracker(loadProgress) {
-  var flagStr = getFlagString();
-  var progressStr = '0';
-  var versionStr = currentVersion;
-  var isCurrentVersionStr = '1';
-  if (loadProgress) {
-    var storedVersion = localStorage.getItem('version');
-    if (storedVersion && storedVersion != currentVersion) {
-      versionStr = storedVersion;
-      isCurrentVersionStr = '0';
-    }
-    progressStr = '1';
-  }
-
-  //Chrome defaults
-  var h = 550;
-  var w = 1327;
-
-  open(`tracker.html?f=${flagStr}&g=${startingGear}&p=${progressStr}&v=${versionStr}&c=${isCurrentVersionStr}`,
-    '',
-    'width=' + w + ',height=' + h + ',titlebar=0,menubar=0,toolbar=0');
+/**
+ * Creates a new link for the tracker.
+ * @returns {string}
+ */
+function trackerLink() {
+  return `./tracker.html?f=${getFlagString()}&g=0&p=0&v=master&c=1`
 }
 
+/**
+ * Loads recent tracker progress by opening the tracker with the provided true boolean
+ */
 function loadMostRecent() {
   openTracker(true);
 }
 
+/**
+ * Launches a new tracker while providing the false boolean.
+ */
 function launch() {
   openTracker(false);
 }
 
+/**
+ * Loads contents of a file and then opens the tracker with it.
+ */
 function loadFileContents() {
   try {
     var saveData = this.result;
@@ -254,23 +139,60 @@ function loadFileContents() {
   openTracker(true);
 }
 
-function loadFromFileClicked() {
-  var file = this.files[0];
-  var reader = new FileReader();
-  reader.addEventListener('loadend', loadFileContents);
-  reader.readAsText(file);
-  this.value = '';
+/**
+ * Loads a file after it's been uploaded.
+ */
+class loadFromFileClicked {
+  constructor() {
+    var file = this.files[0];
+    var reader = new FileReader();
+    reader.addEventListener('loadend', loadFileContents);
+    reader.readAsText(file);
+    this.value = '';
+  }
 }
 
+/**
+ * Loads File Explorer where the user can import their progress.
+ */
 function loadFromFile() {
   var loadProgressElement = document.getElementById('load-progress');
   loadProgressElement.addEventListener('input', loadFromFileClicked);
   loadProgressElement.click();
 }
 
-$(document).ready(() => {
-  jQuery.get(`./settings.yaml`, f => {
+$(document).ready(() => { // Loads the main page when the document has loaded.
+  jQuery.get(`https://josephanimate2021.github.io/twwr-ap-tracker/settings.yaml`, f => {
     const info = jsyaml.load(f);
-    console.log(f);
+    let html = '';
+    for (const i in info) {
+      html += `<fieldset><legend>${i}</legend><table>${(() => {
+        let html = '<colgroup>';
+        if (
+          Object.keys(info[i]).length > 1
+        ) html += '<col class="text-col"><col class="slider-col"><col class="text-col"><col class="slider-col"><col class="text-col"><col class="slider-col">'
+        else for (let k = 0; k <= Object.keys(info[i]).length; k++) html += '<col class="text-col"><col class="slider-col">'
+        return html + '</colgroup>'
+      })()}<tbody>`;
+      let count = 0;
+      for (const g in info[i]) {
+        const info2 = info[i][g];
+        if (count == 3) {
+          html += '</tr>'
+          count = 0;
+        }
+        if (count == 0) html += '<tr>';
+        html += `<td class="label-text">${g.split("_").map(capWord).join(' ')}</td><td class="slider-container">`;
+        if (info2.true != undefined && info2.false != undefined) html += `<label class="switch">
+          <input id="${g}" type="checkbox"${info2.true > 0 ? ' checked' : ''}>
+          <span class="slider"></span>
+        </label>`;
+        else html += `<div class="select-container"><select id="${g}">${Object.keys(info2).map(v => `<option ${info2[v] > 0 ? ' selected' : ''}>${isNaN(parseInt(v)) ? v.split("_").map(capWord).join(' ') : v}</option>`).join('')}</select></div>`;
+        html += '</td>'
+        count++;
+      }
+      html += `</tbody></table></fieldset>`
+    }
+    $("#gameSettings").html(html);
   })
 })
