56318cc59dd0f883ae1ea69da48e4f103803f9ea
Joseph <100003036+josephanimate2021@users.noreply.github.com>
2025-05-14 09:25:05 -0400

    Update main_logic.js

--- scripts/main_logic.js
+++ scripts/main_logic.js
@@ -23,7 +23,7 @@ function findAPItemElement(itemId) {
     return undefined;
   }
   let elem = lookInElements([document.getElementById("triforce"),document.getElementById("shield")]);
-  elem ||= lookInElements(document.getElementsByClassName("sea-sector"));
+  elem ||= lookInElements(document.getElementsByClassName("treasure-chart"));
   elem ||= lookInElements(document.getElementsByClassName("small-key"));
   elem ||= lookInElements(document.getElementsByClassName("boss-key"));
   elem ||= lookInElements($("#tracker").find("img"));
