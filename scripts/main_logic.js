92396f6284f514d572a9b781d1139bca4313c787
Joseph <100003036+josephanimate2021@users.noreply.github.com>
2025-05-13 23:25:46 -0400

    Update main_logic.js

--- scripts/main_logic.js
+++ scripts/main_logic.js
@@ -134,8 +134,8 @@ $(document).ready(function () { // loads the tracker with AP when the page has l
                       else APFunctions.itemsRecieved[todo.functionCall.name]();
                     }
                   }
-                  if (APItemInfo.location != -2) dataChanged()
-                  else toggleLocationAP(APItemInfo.location)
+                  if (APItemInfo.location != -2) toggleLocationAP(APItemInfo.location)
+                  else dataChanged()
                 }
                 displayMessage(`Successfuly checked ${info2.items.length} items that were recieved from AP!`);
               }
